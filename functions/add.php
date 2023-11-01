ffc4f40190d19e15db42fa426be7020d09e31914
Kathleen Kate Santiago <145891887+kathleenkate16@users.noreply.github.com>
2023-11-01 20:36:55 +0700

    fixing
    fixing some error

--- functions/add.php
+++ functions/add.php
@@ -5,27 +5,32 @@
 </head>
 <body>
     <?php
-        include_once("./dbConnection/mysqlconfig_connection.php");
+    include_once("./dbConnection/mysqlconfig_connection.php");
 
-        if(isset($_POST['Submit'])) {
-            $code = $_POST['code'];
-            $name = $_POST['name'];
+    if (isset($_POST['Submit'])) {
+        $code = $_POST['code'];
+        $name = $_POST['name'];
 
-            if(empty($code) || empty($name)) {
-                if(empty($code)) {
-                    echo "<font color='red'>Subject Code Field is empty.</font><br/>";
-                }
-                if(empty($name)) {
-                    echo "<font color='red'>Subject Name Field is empty.</font><br/>";
-                }
-                echo "<br/><a href='javascript:self.history.back();'>Bo Back</a>";
+        if (empty($code) || empty($name)) {
+            if (empty($code)) {
+                echo "<font color='red'>Subject Code Field is empty.</font><br/>";
             }
-            else {
-                $result = mysqli_query($dbc, "INSERT INTO tblsubject(subject_code, subject_name) VALUES ('$code','$name')");
-                echo "<font color='green'>Data added successfully.";
-                echo "<br/><a href='../index.php'>View Result</a>";
+            if (empty($name)) {
+                echo "<font color='red'>Subject Name Field is empty.</font><br/>";
+            }
+            echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
+        } else {
+            $result = mysqli_query($dbc, "INSERT INTO tblsubject (subject_code, subject_name) VALUES ('$code', '$name')");
+
+            if ($result) {
+                echo "<font color='green'>Data added successfully.</font>";
+                echo "<br/><a href='../index.php'>View Results</a>";
+            } else {
+                echo "<font color='red'>Error: " . mysqli_error($dbc) . "</font><br/>";
+                echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
             }
         }
+    }
     ?>
 </body>
 </html>
